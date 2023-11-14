9bedd74fe58e8f03a370decc5b17cb9ce377a258
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-14 19:22:36 +0100

    install Native Reat & Xcode

---
+++ App/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text >My First Native React App</Text>
+      <Text>COST MANAGEMENT</Text>
+      <StatusBar style="auto" />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
