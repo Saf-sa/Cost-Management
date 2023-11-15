8ec59c9cf5058707c2bea62b908f9fc0a0a87f73
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-15 15:40:12 +0100

    create Custominput.js & style login form

--- App/shared/components/ui/CustomInput.js
+++ App/shared/components/ui/CustomInput.js
@@ -1,12 +1,53 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet} from 'react-native'
 import React from 'react'
 
 const CustomInput = () => {
   return (
-    <View>
-      <Text>CustomInput</Text>
+    <View style={StyleSheet.container}>
+      <Text style={styles.text}>Email</Text>
+        <TextInput style={styles.input}  placeholder='Your Email' />
+        <Text style={styles.errorMsg}>Error</Text>
+        <Text style={styles.text}>Password</Text>
+        <TextInput  style={styles.input} placeholder='Your Password' />
+        <Text style={styles.errorMsg}>Error</Text>
     </View>
   )
 }
 
 export default CustomInput
+
+const styles = StyleSheet.create({ 
+
+    container: {
+        width: '100%',
+        marginBottom: 0,
+
+},
+
+    text: {
+        color: '#0283a8',
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+
+
+    },
+
+    input: {
+        marginBottom: 10,
+        marginTop: 5,
+        padding: 10,
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        borderWidth: 1, 
+        borderColor: '#0283a8',
+    },
+    errorMsg:{
+        color: '	#BEBEBE',
+        margintop: 10,
+        marginBottom: 20,
+        fontWeight: 'bold',
+
+
+    },
+    });
