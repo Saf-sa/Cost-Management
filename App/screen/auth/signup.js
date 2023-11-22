4734e70338113fab5f482e9e6024fc1a758a6cc9
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-11-22 10:46:04 +0100

    Add formErrors  for each input signup.js

--- App/screen/auth/signup.js
+++ App/screen/auth/signup.js
@@ -43,6 +43,16 @@ const Signup = () => {
     const [lastName, setlastName] = useState("");
     const [email, setEmail] = useState("");
   const navigation = useNavigation();
+
+  const [formErrors, setFormErrors] = useState({
+    FirstName: null,
+    lastName: null,
+    email: null,
+    password: null,
+    confirmPassword: null,
+  });
+
+  
   const [formData, setFormData] = useState({
     FirstName: "",
     lastName: "",
@@ -59,16 +69,28 @@ const Signup = () => {
   };
 
 
-  const handleSubmit = () => {
-    if (formIsValid(formData)) {
-      console.warn("Successfully registered");
-      navigation.navigate("Login");
-    } else {
-      console.warn("Invalid Form");
-    }
-  };
-
-
+const handleSubmit = () => {
+  if (formIsValid(formData)) {
+    console.warn("Successfully registered");
+    navigation.navigate("Login");
+  } else {
+    setFormErrors({
+      FirstName: !isValidFirstName(formData.FirstName)
+        ? "Invalid first name"
+        : null,
+      lastName: !isValidlastName(formData.lastName)
+        ? "Invalid last name"
+        : null,
+      email: !isValidEmail(formData.email) ? "Invalid email" : null,
+      password: !isValidPassword(formData.password) ? "Invalid password" : null,
+      confirmPassword:
+        formData.password !== formData.confirmPassword
+          ? "Passwords do not match"
+          : null,
+    });
+    console.warn("Invalid Form");
+  }
+};
 
   return (
     <View style={styles.root}>
@@ -80,13 +102,15 @@ const Signup = () => {
           onChangeText={(value) => handleChange(value, "FirstName")}
           placeholder="Your First Name (min 3 letters) "
           secure={false}
+          errorMessage={formErrors.FirstName}
         />
         <CustomInput
           label="last Name"
           value={formData.lastName}
-          onChangeText={(value) => handleChange(value, "lastName")}
+          onChangeText={(value) => handleChange(value, "LastName")}
           placeholder="Your Last Name (min 3 letters)"
           secure={false}
+          errorMessage={formErrors.lastName}
         />
         <CustomInput
           label="Email"
@@ -94,6 +118,7 @@ const Signup = () => {
           onChangeText={(value) => handleChange(value, "email")}
           placeholder="Your Email"
           secure={false}
+          errorMessage={formErrors.email}
         />
         <CustomInput
           label="Password"
@@ -101,6 +126,7 @@ const Signup = () => {
           onChangeText={(value) => handleChange(value, "password")}
           placeholder="1 number & 1 special digits & min 8 with 1 capital letter"
           secure={true}
+          errorMessage={formErrors.password}
         />
         <CustomInput
           label="Confirm Password"
@@ -108,6 +134,7 @@ const Signup = () => {
           onChangeText={(value) => handleChange(value, "confirmPassword")}
           placeholder="Confirm Password"
           secure={true}
+          errorMessage={formErrors.confirmPassword}
         />
         {/* input area  End*/}
 
@@ -116,6 +143,7 @@ const Signup = () => {
           onPress={handleSubmit}
           style={styles.button}
           buttonText={"Register"}
+          errorMessage={formErrors.FirstName}
         />
         {/* Button End */}
       </View>
