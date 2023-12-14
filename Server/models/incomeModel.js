1645e57fd2e7854b500ff3657d15ba8d01489991
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-12-14 18:44:21 +0100

    add & setup incomeModel.js in BE

---
+++ Server/models/incomeModel.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+// use mongoose to create a schema to define the structure of the data
+const incomesSchema = new mongoose.Schema(
+  {
+    date: {
+      type: String,
+      default: Date,
+      required: true,
+    },
+    categories: {
+      type: String,
+      enum: [
+        "Saleary",
+        "taxes refund",
+        "Bonus",
+        "loan",
+        "Sales",
+        "Gift",
+        "Rent",
+        "Allowance",
+        "Refund",
+        "Gambling",
+        "Stocks",
+        "Óther",
+
+      ],
+      required: true,
+      default: "other",
+    },
+
+    otherCategories: {
+      type: String,
+    },
+
+    label: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+  },
+  // add timestamps to the schema to know when document was created or modified
+  { timestamps: true }
+);
+
+// use mongoose to create a model from the schema and export it
+
+const Incomes = mongoose.model("Incomes", incomesSchema);
+
+export default Incomes;
