c648f51b53178fa6e0f41aba91cfdb2e09c71f0a
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-12-23 18:52:26 +0100

    setuo calculator.js base

--- App/screen/Pages/Toolkits/Calculator.js
+++ App/screen/Pages/Toolkits/Calculator.js
@@ -1,12 +1,82 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, SafeAreaView , onPress } from 'react-native'
+import { useState } from 'react';
+import { StatusBar } from 'react-native'; 
 
 const Calculator = () => {
-  return (
-    <View>
-      <Text>Calculator</Text>
-    </View>
-  )
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+const onButtonPress =(value) => { 
+  if (value === "=") {
+    try{
+      setResult(eval(input))
+    }
+    catch (error){
+      setResult("error")
+    }
+  }
+  else if (value === "C") {
+    setInput("");
+    setResult(""); 
+  }
+else {
+  setInput(input + value);
+}
 }
 
-export default Calculator
+  return (
+    <SafeAreaView style={styles.container}>
+
+      <StatusBar style="auto" />
+
+    <View style={styles.resultContainer}>
+      <Text style={styles.resultText}></Text>
+    </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.InputText} 
+        value={input} 
+        onChangeText={setInput}
+        keyboardType='numeric'
+        />
+
+       <View style={styles.buttonContainer}>
+
+        {["7", "8", "9", "/","4","5","6","*","1", "2", "3", "-", "0", "C", "=", "+"].map(
+          (item,index) => (
+         
+         <TouchableOpacity style={styles.button}
+          key={index}
+          onPress={() => onButtonPress(item)}
+          >
+            <Text style={styles.ButtonText}>{item}</Text>
+          
+          </TouchableOpacity>
+
+
+        
+        ) 
+        )}
+        
+        </View>
+         </View>
+    
+    </SafeAreaView>
+  );
+};
+
+export default Calculator;
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+  },
+
+  resultContainer : {
+    flex: 2,
+    justifyContent: 'center',
+    aloignItems: 'flex-end',
+   
+  },
+   
+
+});
