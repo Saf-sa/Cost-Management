ca6e0bb262456a06e14cb5b499da4b50306d4323
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2024-01-03 10:06:42 +0100

    add datePicker expire date  AddReminder.js

--- App/screen/Pages/Toolkits/AddReminder.js
+++ App/screen/Pages/Toolkits/AddReminder.js
@@ -8,7 +8,9 @@ import CustomButton from "../../../shared/components/ui/CustomButton";
 export default function AddReminder() {
     const [date, setDate] = useState();
     const [expireDate, setExpireDate] = useState(false);
-    
+    const [selectedEmail, setSelectedEmail] = useState("");
+    const [contractName, setContractName] = useState('');
+    const [selectedLabel, setSelectedLabel] = useState('');
     const navigation = useNavigation();
 
 
@@ -47,8 +49,38 @@ export default function AddReminder() {
                 }
                 
             </View>
-         
-            
+           <DatePicker
+                value={expireDate}
+                width={"80%"}
+                fontSize={19}
+                height={150}
+                onChange={(expireDate) => setExpireDate(expireDate)}
+                format={"yyyy-mm-dd"}
+            />
+                 <Text style={styles.title}> Contract expire Date</Text>
+               <View style={styles.expireDate}>
+                
+                {
+                    [
+                        {title: "Day", value: expireDate ? expireDate.getDate() : "?"},
+                        {title: "Month", value: expireDate ? expireDate.getMonth() + 1 : "?"},
+                        {title: "Year", value: expireDate ? expireDate.getFullYear() : "?"},
+                    ]
+                    .map((el, index) => {
+                        return (
+                            <View style={styles.datePart} key={index}>
+
+                                <Text style={styles.title}>{el.title}</Text>
+                                <Text style={styles.digit}>{el.value}</Text>
+
+                                 </View>
+   
+                        )
+                    })
+                }
+            </View> 
+
+      
         </View>
     );
 }
