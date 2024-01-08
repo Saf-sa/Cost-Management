044b55ab70087c234a43c3f407dc27f3ea13a709
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2024-01-08 08:23:35 +0100

    fetch data from BE in  Viewexpenses.js

--- App/screen/Pages/ViewExpenses.js
+++ App/screen/Pages/ViewExpenses.js
@@ -1,4 +1,12 @@
-import { View, Text, StyleSheet,TouchableOpacity, DatePickerIOS ,   ScrollView, } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  DatePickerIOS,
+  ScrollView,
+  FlatList
+} from "react-native";
 import React, { useState, useRef, useEffect } from "react";
 import Toast from "react-native-toast-message";
 import { useNavigation } from "@react-navigation/native";
@@ -6,159 +14,107 @@ import AuthHeader from "../../shared/components/AuthHeader";
 import CustomInputSingup from "../../shared/components/ui/CustomInputSignup";
 import CustomButton from "../../shared/components/ui/CustomButton";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import Icon from "../../shared/components/IncomExpenseComponent/Icon";
+import AppText from "../../shared/components/uiApp/AppText";
+import UserNav from "../nav/UserNav";
+import Screen2 from "../../shared/components/Screen";
 import moment from "moment";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 /*  import { REACT_APP_BE_URL } from "../../.env"; */
 import axios from "axios";
+import MyIncome from "./MyIncomes";
 
 // comment this line because solution not found if using .env file
 // go to ligne 84
 //import { API_URL, API_TOKEN } from "@env";
 /*  import { REACT_APP_BE_URL } from "../../.env";  */
 
-const isValidDate = (date) => {};
-
-const isValidCategories = (categories) => {};
-
-const isValidlabel = (label) => {};
-
-const isValidAmount = (amount) => {};
-
-// check all value is valid
-const formIsValid = (DataObj) => {
-  return (
-    Object.values(DataObj).every((value) => value.trim().length > 0) && // check all value is not empty
-    isValidDate(DataObj.date) &&
-    isValidCategories(DataObj.categories) &&
-    isValidlabel(DataObj.label) &&
-    isValidAmount(DataObj.amount)
-  );
-};
-//
-const MyExpense = () => {
-  const [date, setDate] = useState("");
-  const [categories, setCategories] = useState("");
-  const [label, setLabel] = useState("");
-  const [amount, setAmount] = useState("");
+const ViewExpenses = () => {
+  const [storedIncomes, setStoredIncomes] = useState([]);
   const navigation = useNavigation();
-  const [formErrors, setFormErrors] = useState({
-    date: null,
-    categories: null,
-    label: null,
-    amount: null,
-  });
-
-  const [formData, setFormData] = useState({
-    date: null,
-    categories: null,
-    label: "",
-    amount: "",
-  });
-  const handleChange = (value, type) => {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [type]: value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // update formData with form values
-    setFormData({
-      date: date,
-      categories: categories,
-      label: label,
-      amount: amount,
-    });
-
-    if (!formIsValid(date, categories, label, amount)) {
-      Toast.show({
-        type: "success",
-        position: "bottom",
-        text1: "Please add a new expense",
-        visibilityTime: 3000,
-        autoHide: true,
-      });
-
-    }
-
-  };
-
-  useEffect(()=>{
-    const getExpenses = async () => {
+  useEffect(() => {
+    const getIncomes = async () => {
       try {
-          const user = JSON.parse(await AsyncStorage.getItem("@storage_Key"));
-
-        const { data } = await axios.get(
-          `http://localhost:5555/api/expenses/`,
-          {
-            headers: {
-              Authorization: `Bearer ${user.token}`,
-            },
-          }
-        );
-        console.log(data);
+        const incomes = await AsyncStorage.getItem('incomes');
+        if (incomes) {
+          const parsedIncomes = JSON.parse(incomes);
+          setStoredIncomes(parsedIncomes.incomes); // Vérifiez la structure des données ici
+        }
       } catch (error) {
         console.log(error);
       }
     };
-    getExpenses();
-  })
+    getIncomes();
+  }, []);
+  
+let index = 1;
 
   return (
-    <View style={styles.root}>
-      <AuthHeader subtext="All your Expense" />
-      <View style={styles.content}>
-        <CustomInputSingup
-          label="Date"
-          value={formData.date}
-          onChangeText={(value) => handleChange(value, "date")}
-          secure={false}
-        />
-        <CustomInputSingup
-          label="Date"
-          value={formData.categories}
-          onChangeText={(value) => handleChange(value, "Date")}
-          secure={false}
-        />
-        <CustomInputSingup
-          label="Date"
-          value={formData.label}
-          onChangeText={(value) => handleChange(value, "Date")}
-          secure={false}
-        />
-        <CustomInputSingup
-          label="Date"
-          value={formData.amount}
-          onChangeText={(value) => handleChange(value, "Date")}
-          secure={false}
-        />
-        {/* input area  End*/}
-        {/* Button Start */}
-        <CustomButton
-          style={styles.button}
-          buttonText={"Add Expense"}
-          onPress={() => navigation.navigate("MyExpenses")}
-        />
-        {/* Button End */}
+     <ScrollView
+    
+      >
+
+    
+    
+      
+    {storedIncomes.map((income, index) => (
+      
+      <View key={index} style={styles.incomeContainer}>
+
+         
+        <View style={styles.row}>
+          <Text>Date : {new Date(income.date).toISOString().split('T')[0]}</Text>
+          <Text>Categories : {income.categories.join(', ')}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text>Label : {income.label}</Text>
+          <Text>Amount = + {income.amount}</Text>
+        </View>
       </View>
-      <Toast />
-    </View>
+       
+    ))}
+    
+  </ScrollView>
   );
 };
 
-export default MyExpense;
-
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
+  incomeContainer: {
+    marginTop: -30,
+    width: "96%",
+
+    borderWidth: 1,
+    borderColor: "#E0AA3E",
+    borderRadius: 10,
+    padding: 12,
+    marginBottom: 40,
+    marginVertical: 8,
+    backgroundColor: "#F7F7F7",
+   
   },
-  content: {
-    flex: 2,
-    padding: 20,
+  row: {
+    
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingRight: 8,
+    paddingBottom: 8,
   },
-  button: {
-    marginTop: 20,
+  textLabel: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    paddingRight: 8,
+    paddingBottom: 8,
   },
+  textValue: {
+    marginBottom: 8,
+  },
+
+
+
+    
+
+
+ 
 });
+
+export default ViewExpenses;
