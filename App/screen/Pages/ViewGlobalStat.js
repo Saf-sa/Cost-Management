2a21dd8affd19a246bde0af64476251c579395f7
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2024-03-01 18:27:35 +0100

    add expenses  button in  ViewGlobalStat.js

--- App/screen/Pages/ViewGlobalStat.js
+++ App/screen/Pages/ViewGlobalStat.js
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import Icon from "../../shared/components/IncomExpenseComponent/Icon";
 
 
 import ShowLineChart from "../../shared/StatsView/LineChartExpense";
@@ -18,13 +19,44 @@ import IncomeLineChart from "../../shared/StatsView/LineChartIncome";
 
 
 
-
+   
   const ViewGlobalStat= () => {
-
+ const navigation = useNavigation();
   return (
     <View>
       <Text style={styles.TitleExpense}>Expenses</Text>
       <ShowLineChart/>
+        <View style={styles.dashboard}>
+          <TouchableOpacity onPress={() => navigation.push("ViewIncomes")}>
+            <Icon
+              name="dollar-sign"
+              size={56}
+            />
+            <Text style={{ fontSize: 15, paddingTop: 5, textAlign:"center"}}> Income</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.push("ViewExpenses")}>
+            <Icon
+              name="shopping-cart"
+              size={56}
+        
+             
+            />
+            <Text style={{ fontSize: 15, paddingTop: 5, textAlign:"center" }}>Expense</Text>
+          </TouchableOpacity>
+          
+         <TouchableOpacity onPress={() => navigation.push("Forecast")}>
+            <Icon
+              name="file-invoice-dollar"
+              size={56}
+              
+             
+             
+            />
+            <Text style={{ fontSize: 15, paddingTop: 5, textAlign:"center" }}>Forecast</Text>
+          </TouchableOpacity>
+     
+ 
+        </View>
       
       <Text style={styles.TitleIncome}>Incomes</Text>
       <IncomeLineChart/>
@@ -38,6 +70,14 @@ import IncomeLineChart from "../../shared/StatsView/LineChartIncome";
 };
 
 const styles = StyleSheet.create({
+    dashboard: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    marginTop: -50,
+    alignItems: "center",
+  },
 TitleExpense: {
     color: "#E0AA3E",
     fontSize: 20,
@@ -51,6 +91,7 @@ TitleExpense: {
     fontWeight: "bold",
     textAlign: "center",
     marginTop: 20,},
+    
 
 
 
