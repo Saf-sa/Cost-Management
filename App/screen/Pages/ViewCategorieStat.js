562b433b6a07ffcb3fae08acf8afed99ad2e0e54
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2024-03-02 17:17:46 +0100

    Create  ViewCategorielStat.js

---
+++ App/screen/Pages/ViewCategorieStat.js
@@ -0,0 +1,134 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  Toast,
+  useWindowDimensions,
+  TouchableOpacity,
+   ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "../../shared/components/IncomExpenseComponent/Icon";
+
+
+
+
+
+
+   
+  const ViewCatStat= () => {
+ const navigation = useNavigation();
+  return (
+    <View>
+      <Text style={styles.TitleExpense}>Expenses</Text>
+      <ShowLineChart/>
+        <View style={styles.dashboard}>
+          <TouchableOpacity onPress={() => navigation.push("ViewIncomes")}>
+            <Icon
+              name="chart-pie"
+              size={60}
+            />
+            <Text style={{ fontSize: 14, paddingTop: 5, textAlign:"center"}}>Category</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.push("ViewExpenses")}>
+            <Icon
+              name="chart-line"
+              size={60}
+        
+             
+            />
+            <Text style={{ fontSize: 14, paddingTop: 5, textAlign:"center" }}>Week</Text>
+          </TouchableOpacity>
+          
+         <TouchableOpacity onPress={() => navigation.push("Forecast")}>
+            <Icon
+              name="chart-bar"
+              size={60}
+              
+             
+             
+            />
+            <Text style={{ fontSize: 14, paddingTop: 5, textAlign:"center" }}>Month</Text>
+          </TouchableOpacity>
+     
+ 
+        </View>
+      
+      <Text style={styles.TitleIncome}>Incomes</Text>
+      <IncomeLineChart/>
+     <View style={styles.dashboard}>
+          <TouchableOpacity onPress={() => navigation.push("ViewIncomes")}>
+            <Icon
+              name="chart-pie"
+              size={60}
+            />
+            <Text style={{ fontSize: 13, paddingTop: 5, textAlign:"center"}}> Category</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.push("ViewExpenses")}>
+            <Icon
+              name="chart-line"
+              size={60}
+        
+             
+            />
+            <Text style={{ fontSize: 14, paddingTop: 5, textAlign:"center" }}>Week</Text>
+          </TouchableOpacity>
+          
+         <TouchableOpacity onPress={() => navigation.push("Forecast")}>
+            <Icon
+              name="chart-bar"
+              size={60}
+              
+             
+             
+            />
+            <Text style={{ fontSize: 14, paddingTop: 5, textAlign:"center" }}>Month</Text>
+          </TouchableOpacity>
+     
+ 
+        </View>
+
+    
+    </View>
+
+   
+
+  )
+};
+
+const styles = StyleSheet.create({
+    dashboard: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 19,
+    marginLeft: 9,
+    marginTop: -30,
+    alignItems: "center",
+  },
+TitleExpense: {
+    color: "#E0AA3E",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 30,
+    marginBottom: 20,
+  
+  },
+
+  TitleIncome: {
+    color: "#3ABD0D",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 30,
+    marginBottom: 20,
+  },
+    
+
+
+
+});
+
+export default ViewCatStat;
