9a3da28880e0695f79aab40751abfc8a8a13fb5b
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2024-03-04 15:30:54 +0100

    clean code app.js & ViewIncomes.js

--- App/screen/Pages/ViewIncomes.js
+++ App/screen/Pages/ViewIncomes.js
@@ -4,32 +4,15 @@ import {
   StyleSheet,
   TouchableOpacity,
   ScrollView,
-  DatePickerIOS,
-  FlatList
 } from "react-native";
 import React, { useState, useEffect } from "react";
-import Toast from "react-native-toast-message";
 import { useNavigation } from "@react-navigation/native";
-import AuthHeader from "../../shared/components/AuthHeader";
-import CustomInputSingup from "../../shared/components/ui/CustomInputSignup";
-import CustomButton from "../../shared/components/ui/CustomButton";
-import DateTimePicker from "@react-native-community/datetimepicker";
-import Icon from "../../shared/components/IncomExpenseComponent/Icon";
-import AppText from "../../shared/components/uiApp/AppText";
 import UserNav from "../nav/UserNav";
 import Screen2 from "../../shared/components/Screen";
-import moment from "moment";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import axios from "axios";
-import { Colors } from "react-native/Libraries/NewAppScreen";
-/*  import { REACT_APP_BE_URL } from "../../.env"; */
 
-
-// comment this line because solution not found if using .env file
-// go to ligne 84
-//import { API_URL, API_TOKEN } from "@env";
-/*  import { REACT_APP_BE_URL } from "../../.env";  */
 
 const ViewIncomes = ({route}) => {
   const [storedIncomes, setStoredIncomes] = useState([]);// State to store data from AsyncStorage
@@ -129,33 +112,19 @@ const styles = StyleSheet.create({
 backgroundColor: "#F8F4D7",
   },
   incomeContainer: {
-    marginTop: -20,
+    marginTop: -40,
     width: "96%",
     borderWidth: 1,
     borderColor: "#E0AA3E",
     borderRadius: 10,
     padding: 12,
-    marginBottom: 40,
-    marginVertical: 8,
-   
-   
+    marginBottom: 60,
+    
   },
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingRight: 2,
-    paddingBottom: 8,
-  },
-
-  textLabel: {
-    fontWeight: 'bold',
-    marginBottom: 4,
-    paddingRight: 8,
-    paddingBottom: 8,
-  },
-
-  textValue: {
-    marginBottom: 8,
+    marginBottom: 8
   },
 
   button: {
@@ -173,7 +142,7 @@ backgroundColor: "#F8F4D7",
     borderRadius: 8,
     padding: 12,
     textAlign: "center",
-    top: -80,
+    top: -70,
   },
     textButton:{
       color: "#E0AA3E",
@@ -181,7 +150,7 @@ backgroundColor: "#F8F4D7",
       fontSize: 15,
       textAlign: "center",
     },
-        textAmount:{
+      textAmount:{
       color: "green",
       fontWeight: "bold",
       fontSize: 20,
