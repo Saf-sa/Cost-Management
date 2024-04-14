068da9c520dcd1d538752d43a574b054a91019ca
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2024-04-14 21:32:11 +0200

    implemant hook expene&income in history.js

--- App/screen/Pages/History.js
+++ App/screen/Pages/History.js
@@ -4,102 +4,34 @@ import {
   StyleSheet,
   ScrollView,
 } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import React, { useState, useEffect } from "react";
 import AppText from "../../shared/components/uiApp/AppText";
 import UserNav from "../nav/UserNav";
 import Screen2 from "../../shared/components/Screen";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import axios from "axios";
+import { useGetExpenses } from '../../shared/components/IncomExpenseComponent/GetExpense';
+import { useGetIncomes } from '../../shared/components/IncomExpenseComponent/GetIncome';
 
 
 
 export default  ViewAll = ({route}) => {
-  const [storedExpenses, setStoredExpenses] = useState([]);// State to store data from AsyncStorage
-    const [storedIncomes, setStoredIncomes] = useState([]);// State to store data from AsyncStorage
 
-
-  const {category} = route.params;// Get category from MyIcomes.js  
+  const { category= 'all' } = route.params;
+  const incomes = useGetIncomes(category);
+  const expenses = useGetExpenses(category);
+  const navigation = useNavigation(); 
 /*   console.log('category from ViewExpenses ', category); */
-  
-  useEffect(() => {// UseEffect to get data from AsyncStorage
-    const getExpenses = async () => {
-      try {
-           const user = JSON.parse(await AsyncStorage.getItem("@storage_Key"));// Get user data from AsyncStorage
-          /*  console.log('user token ',user.token);  */
-        const { data } = await axios.get(
-          
-           /*  console.log('data ', data), */
-          `http://localhost:5555/api/expenses/${category}`,// Get data in DB collection from backend in DB
-                 /*    console.log('data category from backend  :', category), */
-          {
-            headers: {
-              Authorization: `Bearer ${user.token}`,// Send token to backend
-            },
-            
-          }
-        );
-         // Stocker les données récupérées dans AsyncStorage
-       await AsyncStorage.setItem('expenses', JSON.stringify(data));// Store data in AsyncStorage
-         /*      console.log('data received from Backend ',data);  */
-
-        //await AsyncStorage.clear('expenses')
-
-        const expenses = await AsyncStorage.getItem('expenses');// Get data from AsyncStorage
-        if (expenses) {
-       const parsedExpenses = JSON.parse(expenses);// Parse data from AsyncStorage
-          setStoredExpenses(parsedExpenses.expenses); // Send data to the state
-               /*  console.log('parsedExpenses FrontEnd side ',parsedExpenses);   */
-        }
-      } catch (error) {// Error handling
-     /*    console.log(error);// Error handling */
-      }
-    };
-
-    const getIncomes = async () => {
-      try {
-           const user = JSON.parse(await AsyncStorage.getItem("@storage_Key"));// Get user data from AsyncStorage
-          /*  console.log('user token ',user.token);  */
-        const { data } = await axios.get(
-          
-           /*  console.log('data ', data), */
-          `http://localhost:5555/api/incomes/${category}`,// Get data in DB collection from backend in DB
-                 /*    console.log('data category from backend  :', category), */
-          {
-            headers: {
-              Authorization: `Bearer ${user.token}`,// Send token to backend
-            },
-            
-          }
-        );
-         // Stocker les données récupérées dans AsyncStorage
-       await AsyncStorage.setItem('incomes', JSON.stringify(data));// Store data in AsyncStorage
-         /*      console.log('data received from Backend ',data);  */
-
-        //await AsyncStorage.clear('incomes')
-
-        const incomes = await AsyncStorage.getItem('incomes');// Get data from AsyncStorage
-        if (incomes) {
-       const parsedIncomes = JSON.parse(incomes);// Parse data from AsyncStorage
-          setStoredIncomes(parsedIncomes.incomes); // Send data to the state
-               /*  console.log('parsedExpenses FrontEnd side ',parsedExpenses);   */
-        }
-      } catch (error) {// Error handling
-        /* console.log(error);// Error handling */
-      }
-    };
-     getIncomes();
-    getExpenses();// Call the function to get data from AsyncStorage
+  useEffect(() => {
+    // Call the function to get data from useGetIncomes
   }, []);
-
   
-  const calculateTotalIncomes = storedIncomes.reduce((total, income) => total + Number(income.amount), 0);
+  const calculateTotalIncomes = incomes.reduce((total, income) => total + Number(income.amount), 0);
   
-  const calculateTotalExpenses = storedExpenses.reduce((total, expense) => total + Number( expense.amount), 0);
+  const calculateTotalExpenses = expenses.reduce((total, expense) => total + Number(expense.amount), 0);
 
-  const totalExpenseIncome = calculateTotalIncomes - calculateTotalExpenses;
+  const all = expenses.concat(incomes);
 
-  const storeAll = storedExpenses.concat(storedIncomes);
+all.sort((a, b) => new Date(a.date) - new Date(b.date));
 
   let index = 1;// index for scrollview
 
@@ -132,7 +64,7 @@ export default  ViewAll = ({route}) => {
 </AppText>
 
 
-{storeAll.map((item, index) => (
+{all.map((item, index) => (
   <View key={index} style={styles.expenseContainer}>
     <View style={styles.row}>
       <Text>Date : {item.date && !isNaN(Date.parse(item.date)) ? new Date(item.date).toISOString().split('T')[0] : 'Invalid date' }</Text>
@@ -140,7 +72,7 @@ export default  ViewAll = ({route}) => {
     </View>
     <View style={styles.row}>
       <Text>Label : {item.label}</Text>
-      <Text style={{color: storedExpenses.includes(item) ? 'red' : 'green'}}> Amount = {item.amount}</Text>
+      <Text style={{color: expenses.includes(item) ? 'red' : 'green'}}> Amount = {item.amount}</Text>
     </View>
   </View>
 ))}
