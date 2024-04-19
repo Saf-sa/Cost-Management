814512779154fa106ce4b822d0e1f37a4c4db886
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2024-04-19 15:20:25 +0200

    update hook AppButton.js

--- App/shared/components/uiApp/AppButton.js
+++ App/shared/components/uiApp/AppButton.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const AppButton = ({ AppButtonText, icon, onPress }) => {
-
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
       <LinearGradient
         style={styles.buttonContainer}
         colors={['#f9f295', '#E0AA3E', '#F7EF8A', '#B88A44']}
         start={{ x: 0.1, y: 0.1 }}
-        end={{ x: 1, y: 3 }}>
-      <View style={styles.icon}>
-          <Icon style={styles.icon} name=/* {"dollar-sign"} */{icon}  />
-          
-        </View> 
+        end={{ x: 1, y: 3 }}
+      >
+        <View style={styles.iconContainer}>
+          <Icon style={styles.icon} name={icon} />
+        </View>
       </LinearGradient>
-  <View style={styles.CaText}>
-   <Text style={styles.buttonTitle}>{AppButtonText}</Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.buttonTitle}>{AppButtonText}</Text>
       </View>
-  
     </TouchableOpacity>
-  
   );
 };
 
@@ -32,34 +29,30 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
     alignItems: 'center',
-    
   },
   buttonContainer: {
     width: 66,
-    height: 61,
+    height: 65,
     borderRadius: 6,
-    borderWidth:0.2,
+    borderWidth: 0.2,
     borderColor: 'brown',
     justifyContent: 'center',
     alignItems: 'center',
-
   },
-  CaText: {
-    color: 'black',
-
-  },  
-
+  textContainer: {
+    marginTop: 5,
+  },
   buttonTitle: {
     color: 'black',
     fontSize: 13,
-     alignItems: 'center',
-    justifyContent: 'center',
   },
-
-  icon: {
+  iconContainer: {
     color: 'black',
     fontSize: 30,
-    textAlign: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  icon:{
+    fontSize: 25,
+  }
 });
