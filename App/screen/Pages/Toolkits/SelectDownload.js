2a8854de34f5c153aacf607878fa4edb2ecf4c7e
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2024-04-21 16:22:57 +0200

    add & setup basic SelecteDownload.j

---
+++ App/screen/Pages/Toolkits/SelectDownload.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SelectDownload = () => {
+  return (
+    <View>
+      <Text>SelectDownload</Text>
+    </View>
+  )
+}
+
+export default SelectDownload
