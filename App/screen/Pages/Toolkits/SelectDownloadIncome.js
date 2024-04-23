e1cc2da154620c8415e259e12a5e0b1dc5c2961e
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2024-04-23 16:57:42 +0200

    new update logic in  SelectDownloadincome

--- App/screen/Pages/Toolkits/SelectDownloadIncome.js
+++ App/screen/Pages/Toolkits/SelectDownloadIncome.js
@@ -1,4 +1,4 @@
-import React, { useState, } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput, ScrollView } from "react-native";
 import Toast from "react-native-toast-message";
 import { useNavigation } from "@react-navigation/native";
@@ -7,8 +7,7 @@ import SendButton from '../../../shared/components/uiApp/AppSendButton'
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import moment from "moment";
 import { useGetIncomes } from "../../../shared/components/IncomExpenseComponent/GetIncome";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import usePDFGenerator from "../../../shared/components/uiApp/PdfGenerator";
 
 const isValidDate = (date) => {
   const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19|20)\d\d$/;
@@ -17,7 +16,7 @@ const isValidDate = (date) => {
 
 const SelectDownloadIncome= (route) => {
   const { category = 'all' } = route.params;
-  const expenses = useGetIncomes(category);
+  const incomes = useGetIncomes(category);
   const [selectedStartDate, setSelectedStartDate] = useState('');
   const [selectedEndDate, setSelectedEndDate] = useState('');
   const [isStartDatePickerVisible, setStartDatePickerVisibility] = useState(false);
@@ -114,75 +113,52 @@ const handleConfirmEndDate = (date) => {
   return (
     <View style={styles.root}>
       <Card/>
-         {<View style={styles.content}>
-      <SendButton
-        onPress={handleSubmit}
-        style={styles.button}
-        sendButtonText={"Create PDf"}
-      />
-</View>} 
       <View style={styles.content}>
         <ScrollView style={styles.scrollView}>
           <Text style={styles.category}>Start Date</Text>
-         <TextInput
-  style={styles.inputContainer}
-  label="Start Date"
- value={selectedStartDate}  
-  placeholder="DD/MM/YYYY"
-  secureTextEntry={false}
-  onFocus={showStartDatePicker}
-/>
-<DateTimePickerModal
-  isVisible={isStartDatePickerVisible}
-  mode="date"
-  onConfirm={handleConfirmStartDate}
-  onCancel={hideStartDatePicker}
-/>
+          <TextInput
+            style={styles.inputContainer}
+            label="Start Date"
+            value={selectedStartDate}
+            placeholder="DD/MM/YYYY"
+            secureTextEntry={false}
+            onFocus={showStartDatePicker}
+          />
+          <DateTimePickerModal
+            isVisible={isStartDatePickerVisible}
+            mode="date"
+            onConfirm={handleConfirmStartDate}
+            onCancel={hideStartDatePicker}
+          />
 
-<Text style={styles.category}>End Date</Text>
-<TextInput
-  style={styles.inputContainer}
-  label="date"
-  value={selectedEndDate}  
-  placeholder="DD/MM/YYYY"
-  secureTextEntry={false}
-  onFocus={showEndDatePicker}
-/>
-<DateTimePickerModal
-  isVisible={isEndDatePickerVisible}
-  mode="date"
-  onConfirm={handleConfirmEndDate}
-  onCancel={hideEndDatePicker}
-/>
- <Text style={styles.category}>Catégories</Text>
-        <SelectList
-         dropdownStyles={{
-              borderColor: '#E0AA3E',
-              borderWidth: 1,
-              borderRadius: 6,
-            }}
-            boxStyles={{ borderRadius: 8, borderColor: '#E0AA3E', height: 42, backgroundColor:'white' }}
-          defaultOption={{ value: 'Sélectionner une catégorie' }}
-          data={['incomes']}
-          setSelected={handleChangeCategory}
-        />
-
-        <Text style={styles.category}>Sub-Catégories</Text>
-        <SelectList
-             dropdownStyles={{
-              borderColor: '#E0AA3E',
-              borderWidth: 1,
-              borderRadius: 6,
-            }}
-        boxStyles={{ borderRadius: 8, borderColor: '#E0AA3E', height: 42, backgroundColor:'white' }}
-          defaultOption={{ value: 'Sélectionner une sous-catégorie' }}
-          data={subCategories}
-          setSelected={handleChangeSubCategory}
-        />
-
+          <Text style={styles.category}>End Date</Text>
+          <TextInput
+            style={styles.inputContainer}
+            label="End Date"
+            value={selectedEndDate}
+            placeholder="DD/MM/YYYY"
+            secureTextEntry={false}
+            onFocus={showEndDatePicker}
+          />
+          <DateTimePickerModal
+            isVisible={isEndDatePickerVisible}
+            mode="date"
+            onConfirm={handleConfirmEndDate}
+            onCancel={hideEndDatePicker}
+          />
+          {!pdfData && (
+        <SendButton onPress={handleGeneratePDF} 
+        sendButtonText={"Create PDF"}
+        style={styles.button}
+         />
+      )}
+      {pdfData && (
+        <PDFViewer style={{ flex: 1 }}>
+          <Document data={pdfData} />
+        </PDFViewer>
+      )}
         </ScrollView>
       </View>
-
       <Toast />
     </View>
   );
